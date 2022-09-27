1fc2620e11bf96157c138fe55cef0d10c6c32efd
Wilson Vasquez <wilsonraymundov@gmail.com>
2022-09-27 16:23:34 -0600

    Agregando tabla en modulo corredor

--- src/pages/Corredor/Detalle/corredoruni.jsx
+++ src/pages/Corredor/Detalle/corredoruni.jsx
@@ -9,7 +9,105 @@ const CorredorUni = () => {
     <div className="page-content">
       <Container fluid>
         <div>
-            <h3>Espacio para el Módulo "Corredor"</h3>
+            <h4>Requisición de Materias Primas y Materiales</h4>
+            <label htmlFor="No-Maquina">No. Máquina</label>
+            <input type="text" />
+        <table class="table table-responsive">
+    <thead>
+        <tr>
+            <th>Codigo Material</th>
+            <th>Nombre Material</th>
+            <th>Sacos Entregado</th>
+            <th>Cantidad en Libras</th>
+            <th>Total en Libras</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>
+                <input type="number"  placeholder="Codigo"/>
+            </td>
+            <td>
+                <input type="text"  placeholder="Descripción"/>
+            </td>
+            <td>
+                <input type="text" placeholder="Cantidad Sacos" />
+            </td>
+            <td>
+                <input type="number" placeholder="Libras" />
+            </td>
+            <td>
+                <input type="number" placeholder="Totales" />
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="number"  placeholder="Codigo"/>
+            </td>
+            <td>
+                <input type="text"  placeholder="Descripción"/>
+            </td>
+            <td>
+                <input type="text" placeholder="Cantidad Sacos" />
+            </td>
+            <td>
+                <input type="number" placeholder="Libras" />
+            </td>
+            <td>
+                <input type="number" placeholder="Totales" />
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="number"  placeholder="Codigo"/>
+            </td>
+            <td>
+                <input type="text"  placeholder="Descripción"/>
+            </td>
+            <td>
+                <input type="text" placeholder="Cantidad Sacos" />
+            </td>
+            <td>
+                <input type="number" placeholder="Libras" />
+            </td>
+            <td>
+                <input type="number" placeholder="Totales" />
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="number"  placeholder="Codigo"/>
+            </td>
+            <td>
+                <input type="text"  placeholder="Descripción"/>
+            </td>
+            <td>
+                <input type="text" placeholder="Cantidad Sacos" />
+            </td>
+            <td>
+                <input type="number" placeholder="Libras" />
+            </td>
+            <td>
+                <input type="number" placeholder="Totales" />
+            </td>
+        </tr>
+        <br />
+        <br />
+        <tr>
+            <th>Solicitado por</th>
+            <th>Entregado Por</th>
+        </tr>
+        <tr>
+            <td>
+                <input type="text" />
+            </td>
+            <td>
+                <input type="text" />
+            </td>
+        </tr>
+        
+    </tbody>
+</table>
         </div>
       </Container>
     </div>
