399231a6dd7022d6e6a09d31818a040c60a8dd43
Wilson Vasquez <wilsonraymundov@gmail.com>
2022-09-27 15:43:07 -0600

    Modulo Isrel agregado

---
+++ src/pages/Israel/IsraelRuben/israelruben.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+    Container,
+  } from "reactstrap";
+
+const israelruben = () => {
+  return (
+    <div className="page-content">
+         <Container fluid>
+        <div>
+            <h3>ISRAEL DE LEÓN</h3>
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+export default israelruben
