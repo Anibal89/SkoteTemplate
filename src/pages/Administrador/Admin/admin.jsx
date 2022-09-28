4b962108d70185d2a860a4c574fd752a65a9588f
Wilson Vasquez <wilsonraymundov@gmail.com>
2022-09-28 14:37:27 -0600

    Cambios realizados en Modulo Admin

--- src/pages/Administrador/Admin/admin.jsx
+++ src/pages/Administrador/Admin/admin.jsx
@@ -1,13 +1,48 @@
 import React from 'react'
+
 import {
     Container,
 } from  "reactstrap";
 
+/******************************** */
+
 const admin = () => {
+  
+  const [usuarios, setUsuarios]= React.useState([]);
+
+
+  React.useEffect(() =>{
+    function getData(){
+      fetch("http://localhost:4000/api/rol")
+      .then((res) => res.json())
+      .then ((dato) => setUsuarios(dato) );
+
+    }
+    getData();
+  }, [] );
+  
   return (
     <div className="page-content">
         <Container fluid>
-        <h1>JBSJAHJDH</h1>
+          <tr>
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Estado</th>
+
+          </tr>
+          
+          {/* Mostrando en pantalla los datos de usuarios */}
+          {usuarios && usuarios.map((item) =>
+            <tr key={item.Id_Rol}>
+            <th>{item.Id_Rol}</th>
+            <td>{item.Nombre_Rol}</td>
+            <td>{item.Estado_Rol}</td>
+          
+              </tr>
+
+            
+          )}
+
         </Container>
     </div>
   )
